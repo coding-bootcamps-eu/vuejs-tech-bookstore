f341df4cad5ea820604a80ad21976ffc749b1930
Joe Ray Gregory <joe@may17.de>
2022-01-31 18:36:25 +0100

    Add solution

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <LayoutHeader />
+  <main>
+    <div id="app" class="inside">
+      <BookList />
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import BookList from "./components/BookList.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    LayoutHeader,
+    BookList,
   },
 };
 </script>
 
 <style>
 @import url("@/assets/reset.css");
+
+:root {
+  --primary: #6a1cc3;
+  --primary-dark: #370b68;
+  --secondary: #f39c12;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+h1,
+h2,
+h3,
+h4 {
+  color: var(--primary);
+  margin-bottom: 15px;
+}
+
+.inside {
+  margin: 10px;
+}
+
+@media only screen and (min-width: 1140px) {
+  .inside {
+    width: 960px;
+    margin: 0 auto;
+  }
+}
 </style>
